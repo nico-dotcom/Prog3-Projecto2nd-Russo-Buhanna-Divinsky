45fda062aed6b1736bab6d72214c76a9ea3af7cc
Nico <nicolasrusso004@gmail.com>
2024-11-25 11:30:40 -0300

    Boton de like

---
+++ proyectoIntegrador/src/components/HomePosteo.js
@@ -0,0 +1,154 @@
+import {Text, View, FlatList, StyleSheet,TouchableOpacity, ActivityIndicator} from 'react-native';
+import React, {Component} from 'react';
+import {auth, db} from '../firebase/config';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import firebase from 'firebase';
+export default class HomePage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            like:false        
+        }
+    }
+
+    componentDidMount(){
+        const mailUser = auth.currentUser.email;
+        const verificarLike =  this.props.item.data.like.includes(mailUser);
+        if(verificarLike == true){
+            this.setState({like: true})
+    }}
+
+    like(id){     
+        const mailUser = auth.currentUser.email;
+        console.log("like info" , this.state.likeinfo)
+
+        db.collection('posts')
+            .doc(id)
+            .update({
+                like: firebase.firestore.FieldValue.arrayUnion(mailUser)
+            })
+            .then (()=> {
+                this.setState({like: true});
+                console.log("se likeo correctamente")
+            }),
+            error => {
+                console.error("Salto el siguiente error: ", error);
+                this.setState({loading: false});
+            }}
+                
+            
+
+    dislike(id){
+        const mailUser = auth.currentUser.email;
+
+        db.collection('posts')
+        .doc(id)
+        .update({
+            like: firebase.firestore.FieldValue.arrayRemove(mailUser)
+
+        })
+        .then (()=> {
+            this.setState({like: false});
+            console.log("se deslikeo correctamente")
+        }),
+        error => {
+            console.error("Salto el siguiente error: ", error);
+            this.setState({loading: false});
+    }
+    }
+
+    render() {
+        return (
+
+                            <View style={styles.postContainer}>
+
+                                <Text style={styles.postTitle}>
+                                    Descripción: {this.props.item.data.descripcion}
+                                </Text>
+
+                                <Text style={styles.postDate}>
+                                    Fecha: {this.props.item.data.fecha_creacion}
+                                </Text>
+
+                                <Text style={styles.postLike}>
+                                Likes: {this.props.item.data.like.length}
+                                </Text>
+
+                             
+                                {
+                                this.state.like === true ? (
+                                    <TouchableOpacity style={styles.IrRegistro} onPress={() => this.dislike(this.props.item.id)}>
+                                        <AntDesign name="dislike1" size={24} color="red" />
+                                </TouchableOpacity>
+                                   
+                                ) : (
+                                    <TouchableOpacity style={styles.IrRegistro} onPress={() => this.like(this.props.item.id)}>
+                                    <AntDesign name="like1" size={24} color="green" />
+
+                                </TouchableOpacity>
+                                )
+                            }
+
+
+                            </View>
+                        
+        );
+    }
+    
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f5f5f5',
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom:20,
+    },
+
+    listContainer: {
+        paddingBottom: 20,
+    },
+
+    postContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginBottom:10,
+        marginRight:10,
+        marginLeft:10,
+        padding:10
+    },
+
+    postTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+        color: '#007BFF',
+    },
+
+    postDate: {
+        fontSize: 14,
+        color: '#666',
+    },
+
+    postLike: {
+        fontSize: 14,
+        marginTop: 5,
+        color: '#666',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    IrRegistro:{
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        padding: 8,
+        borderRadius: 5,
+        alignItems: 'center'
+    },
+});
--- proyectoIntegrador/src/screens/Home.js
+++ proyectoIntegrador/src/screens/Home.js
@@ -1,16 +1,127 @@
-import {Text,View, TouchableOpacity} from "react-native";
+import {Text,View,StyleSheet,ActivityIndicator,FlatList} from "react-native";
 import React, {Component} from "react";
-import HomeGet from '../components/HomeGet';  
+import HomePage from '../components/HomePosteo';  
+import {auth, db} from '../firebase/config';
+
+
 
 export default class Home extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            loading: true,
+            posteos: [],
+            error: '',
+        }
+    }
+    componentDidMount(){
+        db.collection('posts')
+        .orderBy("fecha_creacion", "desc")
+        .onSnapshot(
+            docs => {
+                let posts = [];
+                docs.forEach(doc => {
+                    posts.push({
+                        id: doc.id,
+                        data: doc.data()
+                    });
+                });
+                this.setState({
+                    posteos: posts,
+                    loading: false
+                }, ()=> console.log("posteos", this.state.posteos));
+            },
+            error => {
+                console.error("Salto el siguiente error: ", error);
+                this.setState({loading: false});
+            }
+        );
     }
     render() {
         return(
             <View> 
-                <HomeGet navigation ={this.props.navigation}/>
-            </View>
+                {/* --- */}
+                <View style={styles.container}>
+
+<Text style={styles.title}>Home</Text>
+
+{this.state.loading ? (
+    <ActivityIndicator size="large" color="#007BFF" />
+) : (
+
+    <FlatList
+        data={this.state.posteos}
+        keyExtractor={(item) => item.id}
+
+        renderItem={({ item }) => (
+            <HomePage item={item} />
+        )} />
+    )
+    }
+    
+                {/* --- */}
+
+                </View>
+                </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#f5f5f5',
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom:20,
+    },
+
+    listContainer: {
+        paddingBottom: 20,
+    },
+
+    postContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        marginBottom:10,
+        marginRight:10,
+        marginLeft:10,
+        padding:10
+    },
+
+    postTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+        color: '#007BFF',
+    },
+
+    postDate: {
+        fontSize: 14,
+        color: '#666',
+    },
+
+    postLike: {
+        fontSize: 14,
+        marginTop: 5,
+        color: '#666',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    IrRegistro:{
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        padding: 8,
+        backgroundColor: '#007BFF',
+        borderRadius: 5,
+        alignItems: 'center'
+    },
+});
